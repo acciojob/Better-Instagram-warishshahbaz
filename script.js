665c6d95fc16fde73da89d1a1be2e8500272c2ff
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-03-31 16:46:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const pictures = document.querySelectorAll('.image');
-
+document.addEventListener('DOMContentLoaded', function() {
   let draggedItem = null;
 
-  pictures.forEach(picture => {
-    picture.addEventListener('dragstart', () => {
-      draggedItem = picture;
+  document.querySelectorAll('.draggable').forEach(item => {
+    item.addEventListener('dragstart', function() {
+      draggedItem = this;
       setTimeout(() => {
-        picture.style.display = 'block';
+        this.style.display = 'none';
       }, 0);
     });
 
-    picture.addEventListener('dragend', () => {
+    item.addEventListener('dragend', function() {
       setTimeout(() => {
-        draggedItem.style.display = 'block';
+        this.style.display = 'block';
         draggedItem = null;
       }, 0);
     });
-  });
 
-  document.addEventListener('dragover', e => {
-    e.preventDefault();
-  });
+    item.addEventListener('dragover', function(e) {
+      e.preventDefault();
+    });
 
-  document.addEventListener('dragenter', e => {
-    e.preventDefault();
-  });
-
-  document.addEventListener('drop', e => {
-    e.preventDefault();
-    if (draggedItem) {
-      draggedItem.style.display = 'block';
-      if (e.target.classList.contains('image')) {
-        e.target.parentNode.insertBefore(draggedItem, e.target);
-      } else if (e.target.classList.contains('flex')) {
-        e.target.appendChild(draggedItem);
+    item.addEventListener('drop', function() {
+      if (draggedItem !== null && draggedItem !== this) {
+        const temp = this.style.backgroundImage;
+        this.style.backgroundImage = draggedItem.style.backgroundImage;
+        draggedItem.style.backgroundImage = temp;
       }
-    }
+    });
   });
 });
